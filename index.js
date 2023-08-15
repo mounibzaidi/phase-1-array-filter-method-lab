9f665d9ac8eabd60eedf4a10663b19c4aaa7d1e0
zaidi mounib <mounibzaidi@gmail.com>
2023-08-15 14:12:50 +0100

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution here
+const drivers = [
+    {
+        name: 'Bobby',
+        hometown: 'Pittsburgh'
+    },
+    {
+        name: 'Sammy',
+        hometown: 'New York'
+    },
+    {
+        name: 'Sally',
+        hometown: 'Cleveland'
+    },
+    {
+        name: 'Annette',
+        hometown: 'Los Angeles'
+    },
+    {
+        name: 'Bobby',
+        hometown: 'Tampa Bay'
+    }
+];
+
+function findMatching(drivers, query) {
+    return drivers.filter(driver => driver.toLowerCase().includes(query.toLowerCase()));
+}
+
+function fuzzyMatch(drivers, query) {
+    return drivers.filter(driver => driver.toLowerCase().indexOf(query.toLowerCase()) === 0);
+}
+
+function matchName(drivers, query) {
+    return drivers.filter(driver => driver.name.toLowerCase() === query.toLowerCase());
+}
